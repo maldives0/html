28fcd3d4d5484e5b79b0d04d276e62c1383dccfd
maldives0 <67949211+maldives0@users.noreply.github.com>
2020-07-31 13:18:33 +0900

    1

--- javascript/js/18.ex_jumin.js
+++ javascript/js/18.ex_jumin.js
@@ -3,51 +3,47 @@ function init() {
     var f = document.querySelector('.ex_jumin input[name=j01]');
     var b = document.querySelector('.ex_jumin input[name=j02]');
     var result = document.querySelector('.ex_jumin div');
-   
-    
-      var front = f.value;
-            
-     var back = b.value;
-    
-    
-      
-    function jumin(a,b) {
-        gender();
-       
-      
+
+
+    var front = f.value;
+    var back = b.value;
  
+
+
+    function jumin() {
+        
+
         var total = 0;
-        for (var i in a) {
- 
-           a = front[i];
-         b = back[i];  
-      
-            
-            total +=  a * b;
-           
+        for (var i = 0; i < front.length-1; i++) {
+
+            total += front[i] * back[i];
+          
         }
         var cNum = 11 - (total % 11);
 
-        if (cNum == back[12]) {
-            result.textContent = "정상적인 주민번호입니다.";
+        if (cNum == front[12]) {
+            result.innerHTML = "정상적인 주민번호입니다.";
 
         } else {
-            result.textContent = "잘못된 주민번호입니다.";
+            result.innerHTML = "잘못된 주민번호입니다.";
         }
-
+gender();
 
     }
 
     function gender() {
-        var gen = back;
-        var aa = gen[0] == "1" ? "남" : "여";
-        result.innerHTML += '성별:' + aa;
+        var gen = front;
+        
+        var aa = gen[6] == "1" ? "남" : "여";
+         console.log(aa);
+        result.innerHTML += ' 성별 =' + aa;
         //  삼합연산자: if문을 한 줄로 표현한다
         //  ? 앞쪽은 조건 = if(gen[0]==1)
         //      : =  "남" else  "여"
         //    }
     }
     //실행명령              
+
 
 
     //이벤트
